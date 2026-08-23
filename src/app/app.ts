a41bb819be82bc12904c7ccf4a1044da2de954e9
Nícolas Alves Mendes <nicolasmendes05@hotmail.com>
2026-08-23 13:47:50 -0300

    Adiciona tela de login do SecureDesk

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Helpdesk } from './helpdesk/helpdesk';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Helpdesk],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
